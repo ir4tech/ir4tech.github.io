0eae72f0fc8eb34e0ec83f906eb22b39d864f350
Josh Hesketh <josh.hesketh@gmail.com>
2021-09-16 10:59:14 +0100

    Updated from template

---
+++ src/components/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="landing">
+      <h1>Hello</h1>
+      <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Landing'
+}
+</script>
+
+<style>
+
+</style>
